38d4e3362012ec90344fd4df376ae6e7fde7a07c
Prince Kumar Sharma <sittu9129@gmail.com>
2026-02-25 17:08:44 +0530

    Update socket connection URL to production

--- LinkedIn/frontend/src/pages/Network.jsx
+++ LinkedIn/frontend/src/pages/Network.jsx
@@ -8,7 +8,7 @@ import { RxCrossCircled } from "react-icons/rx";
 
 import io from "socket.io-client"
 
-const socket=io("http://localhost:8000")
+const socket=io("https://linkedinbackend-5bne.onrender.com")
 function Network() {
 let {serverUrl}=useContext(authDataContext)
 let [connections,setConnections]=useState([])
