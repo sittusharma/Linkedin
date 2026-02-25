1c6b9e481d6e654cee8414f10725177e693ecf5f
Prince Kumar Sharma <sittu9129@gmail.com>
2026-02-25 11:41:15 +0530

    Fix import statement for post model

--- LinkedIn/backend/controllers/post.Controllers.js
+++ LinkedIn/backend/controllers/post.Controllers.js
@@ -1,4 +1,4 @@
-import post from "../models/post.model.js"
+import Post from "../models/post.model.js"
 import uploadOnCloudinary from "../config/cloudinary.js"
 import { io } from "../index.js";
 import Notification from "../models/notification.model.js";
