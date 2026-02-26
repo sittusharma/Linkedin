81d2ef134f80ed19b48b6307ec85bc44061fc137
Prince Kumar Sharma <sittu9129@gmail.com>
2026-02-26 12:14:15 +0530

    Update index.js

--- LinkedIn/backend/index.js
+++ LinkedIn/backend/index.js
@@ -15,14 +15,14 @@ let app=express()
 let server=http.createServer(app)
 export const io=new Server(server,{
     cors:({
-        origin:"http://localhost:5173",
+        origin:"https://linkedin-frontend-6rdi.onrender.com",
         credentials:true
     })
 })
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://linkedin-frontend-6rdi.onrender.com",
     credentials:true
 }))
 let port=process.env.PORT || 5000
